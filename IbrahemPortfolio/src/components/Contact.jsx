444cb4a879c7e13ca3b34d4f96fd8b397b834915
IbrahemMohammad09 <137792138+IbrahemMohammad09@users.noreply.github.com>
2024-10-03 03:13:27 +0300

    ADD CONTACT

--- IbrahemPortfolio/src/components/Contact.jsx
+++ IbrahemPortfolio/src/components/Contact.jsx
@@ -1,39 +1,135 @@
-import React from 'react'
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+
+  const [formData, setFormData] = useState({
+    name: '',
+    phone: '',
+    email: '',
+    subject: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    
+    emailjs.sendForm('service_cbrkr8s', 'template_9wci25i', e.target, '5vJT1QDohQIqCsVh5')
+      .then((result) => {
+          console.log(result.text);
+          alert('Message Sent Successfully!');
+      }, (error) => {
+          console.log(error.text);
+          alert('Failed to send the message, please try again.');
+      });
+  };
+  
   return (
+
     <div id='contact' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
-        <h1 className='py-4 text-4xl font-bold text-center text-[#001b5e]'>Contact</h1>
-        <form action='' method='POST' encType='multipart/form-data'>
-          <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
-              <div className='flex flex-col'>
-                <label className='uppercase text-sm py-2'>Name</label>
-                <input className='border-2 p-3 rounded-lg flex border-gray-300' type="text" name="name"/>
-              </div>
-              <div className='flex flex-col'>
-                <label className='uppercase text-sm py-2'>Phone</label>
-                <input className='border-2 p-3 rounded-lg flex border-gray-300' type="text" name="phone"/>
-              </div>
-          </div>
-          <div>
-            <div className='flex flex-col'>
-                <label className='uppercase text-sm py-2'>Email</label>
-                <input className='border-2 p-3 rounded-lg flex border-gray-300' type="email" name="email"/>
-            </div>
-          </div>
-          <div className='flex flex-col'>
-              <label className='uppercase text-sm py-2'>Subject</label>
-              <input className='border-2 p-3 rounded-lg flex border-gray-300' type="text" name="subject"/>
-          </div>
-          <div  className='flex flex-col'>
-              <label className='uppercase text-sm py-2'>Message</label>
-              <textarea className='border-2 p3 rounded-lg border-gray-300' rows="10" name='message' ></textarea>
-          </div>
-          <button className='bg-[#001b5e] text-gray-100 mt-4 w-full p-4 rounded-lg'>
-            Send Message
-          </button>
-        </form>
-    </div>
+    <h1 className='py-4 text-4xl font-bold text-center text-[#001b5e]'>Contact</h1>
+    <form onSubmit={sendEmail} method='POST' encType='multipart/form-data'>
+      <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
+        <div className='flex flex-col'>
+          <label className='uppercase text-sm py-2'>Name</label>
+          <input 
+            className='border-2 p-3 rounded-lg flex border-gray-300' 
+            type="text" 
+            name="name" 
+            value={formData.name} 
+            onChange={handleChange} 
+          />
+        </div>
+        <div className='flex flex-col'>
+          <label className='uppercase text-sm py-2'>Phone</label>
+          <input 
+            className='border-2 p-3 rounded-lg flex border-gray-300' 
+            type="text" 
+            name="phone" 
+            value={formData.phone} 
+            onChange={handleChange} 
+          />
+        </div>
+      </div>
+      <div>
+        <div className='flex flex-col'>
+          <label className='uppercase text-sm py-2'>Email</label>
+          <input 
+            className='border-2 p-3 rounded-lg flex border-gray-300' 
+            type="email" 
+            name="email" 
+            value={formData.email} 
+            onChange={handleChange} 
+          />
+        </div>
+      </div>
+      <div className='flex flex-col'>
+        <label className='uppercase text-sm py-2'>Subject</label>
+        <input 
+          className='border-2 p-3 rounded-lg flex border-gray-300' 
+          type="text" 
+          name="subject" 
+          value={formData.subject} 
+          onChange={handleChange} 
+        />
+      </div>
+      <div className='flex flex-col'>
+        <label className='uppercase text-sm py-2'>Message</label>
+        <textarea 
+          className='border-2 p3 rounded-lg border-gray-300' 
+          rows="10" 
+          name='message' 
+          value={formData.message} 
+          onChange={handleChange} 
+        />
+      </div>
+      <button className='bg-[#001b5e] text-gray-100 mt-4 w-full p-4 rounded-lg'>
+        Send Message
+      </button>
+    </form>
+  </div>
+
+
+    // <div id='contact' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
+    //     <h1 className='py-4 text-4xl font-bold text-center text-[#001b5e]'>Contact</h1>
+    //     {/* <form action="https://getform.io/f/bpjmqmeb" method="POST" encType='multipart/form-data'> */}
+    //     <form action="https://getform.io/f/bpjmqmeb" method="POST" encType='multipart/form-data'>
+    //       <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
+    //           <div className='flex flex-col'>
+    //             <label className='uppercase text-sm py-2'>Name</label>
+    //             <input className='border-2 p-3 rounded-lg flex border-gray-300' type="text" name="name"/>
+    //           </div>
+    //           <div className='flex flex-col'>
+    //             <label className='uppercase text-sm py-2'>Phone</label>
+    //             <input className='border-2 p-3 rounded-lg flex border-gray-300' type="text" name="phone"/>
+    //           </div>
+    //       </div>
+    //       <div>
+    //         <div className='flex flex-col'>
+    //             <label className='uppercase text-sm py-2'>Email</label>
+    //             <input className='border-2 p-3 rounded-lg flex border-gray-300' type="email" name="email"/>
+    //         </div>
+    //       </div>
+    //       <div className='flex flex-col'>
+    //           <label className='uppercase text-sm py-2'>Subject</label>
+    //           <input className='border-2 p-3 rounded-lg flex border-gray-300' type="text" name="subject"/>
+    //       </div>
+    //       <div  className='flex flex-col'>
+    //           <label className='uppercase text-sm py-2'>Message</label>
+    //           <textarea className='border-2 p3 rounded-lg border-gray-300' rows="10" name='message' ></textarea>
+    //       </div>
+    //       <button className='bg-[#001b5e] text-gray-100 mt-4 w-full p-4 rounded-lg'>
+    //         Send Message
+    //       </button>
+    //     </form>
+    // </div>
+
+
+
   )
 }
 
--- IbrahemPortfolio/src/components/Sidenav.jsx
+++ IbrahemPortfolio/src/components/Sidenav.jsx
@@ -18,6 +18,7 @@ const Sidenav = () => {
             nav ? (
                 <div className='fixed w-full h-screen bg-wihte/90 flex flex-col justify-center items-center z-20'>
                     <a 
+                        onClick={handleNav}
                         href='#main' 
                         className='w-[75%] flex justify-center items-center rounded-full shadow-lg 
                         bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 '
@@ -26,6 +27,7 @@ const Sidenav = () => {
                         <span className='pl-4'>Home</span>
                     </a>
                     <a 
+                        onClick={handleNav}
                         href='#work' 
                         className='w-[75%] flex justify-center items-center rounded-full shadow-lg 
                         bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 '
@@ -34,6 +36,7 @@ const Sidenav = () => {
                         <span className='pl-4'>Work</span>
                     </a>
                     <a 
+                        onClick={handleNav}
                         href='#projects' 
                         className='w-[75%] flex justify-center items-center rounded-full shadow-lg 
                         bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 '
@@ -42,6 +45,7 @@ const Sidenav = () => {
                         <span className='pl-4'>Projects</span>
                     </a>
                     <a 
+                        onClick={handleNav}
                         href='#main' 
                         className='w-[75%] flex justify-center items-center rounded-full shadow-lg 
                         bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 '
@@ -50,6 +54,7 @@ const Sidenav = () => {
                         <span className='pl-4'>Resume</span>
                     </a>
                     <a 
+                        onClick={handleNav}
                         href='#contact' 
                         className='w-[75%] flex justify-center items-center rounded-full shadow-lg 
                         bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 '
